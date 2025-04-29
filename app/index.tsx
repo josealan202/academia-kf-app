3252896c610709d7b773e1f2092d595df6379764
José Alan <alan.dias@academico@ifpb.edu.br>
2025-04-29 11:33:39 -0300

    Alteração do index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
 import { Text, View, StyleSheet } from "react-native";
 
+const dados = [
+  {
+    id: 1,
+    nome: 'Alan',
+    cor: 'branco'
+  },
+  {
+    id: 2,
+    nome: 'Venícios',
+    cor: 'branco'
+  },
+  {
+    id: 3,
+    nome: 'João Vitor',
+    cor: 'pardo'
+  }    
+]
+
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={style.container}>
+
+      
     </View>
   );
 }
