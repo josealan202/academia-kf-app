c7a722d33543b2323061247745eedb8104ae3d5c
José Alan <alan.dias@academico@ifpb.edu.br>
2025-05-20 11:35:48 -0300

    Modificações no index

--- app/index.tsx
+++ app/index.tsx
@@ -1,35 +1,52 @@
 import { Text, View, StyleSheet } from "react-native";
 
-const dados = [
+const usuarios = [
   {
     id: 1,
-    nome: 'Alan',
-    cor: 'branco'
+    nome: "Jurema",
+    cargo: "Aluna"
   },
   {
     id: 2,
-    nome: 'Venícios',
-    cor: 'branco'
+    nome: "Jubileu",
+    cargo: "Aluno"
   },
-  {
+    {
     id: 3,
-    nome: 'João Vitor',
-    cor: 'pardo'
-  }    
+    nome: "Cicinho Cabeção",
+    cargo: "Personal"
+  }
 ]
 
 export default function Index() {
   return (
-    <View style={style.container}>
+    <View style={style.container}
 
-      
+    >
+      {
+      usuarios.map(item => (
+        <Text style = {{fontSize: 20}}>{item.nome}</Text>
+      ))
+      }
     </View>
   );
 }
-
-const style = StyleSheet.create({
+const style = StyleSheet.create ({
+ 
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-}})
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+
+  itemLista: {
+    width: '100%',
+    padding: 10,
+    marginBottom: 2,
+    backgroundColor: "blue"
+  },
+
+  textoLista:{
+    fontSize: 20
+  }
+})
