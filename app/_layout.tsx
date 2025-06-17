81cd981f79520678b980e8214879da8b2723df4d
José Alan Dias Almeida <alan.dias@academico.ifpb.edu.br>
2025-06-17 14:34:45 +0000

    Alterando index e layout.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,11 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-  <Stack>
-    <Stack.Screen name="index" options={{title: "Academia Gladiadores"}}/>
-  </Stack>
-  )
+    <Stack>
+      <Stack.Screen 
+        name="index"
+        options={{ title: "Home" }}
+      />
+    </Stack>
+  );
 }
-
--- app/index.tsx
+++ app/index.tsx
@@ -1,38 +1,53 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, FlatList } from "react-native";
 
 const usuarios = [
   {
     id: 1,
-    nome: "Jurema",
-    cargo: "Aluna"
+    nome: "André",
+    cargo: "Professor"
   },
   {
     id: 2,
-    nome: "Jubileu",
+    nome: "Cicinho Cabeça",
     cargo: "Aluno"
-  },
-    {
-    id: 3,
-    nome: "Cicinho Cabeção",
-    cargo: "Personal"
   }
-]
+];
 
 export default function Index() {
-  return (
-    <View style={style.container}
 
-    >
-      {
-      usuarios.map(item => (
-        <Text style = {{fontSize: 20}}>{item.nome}</Text>
-      ))
-      }
-    </View>
+  function Item({usuario}) {
+    return (
+      <View key={usuario.id} style={style.itemLista}>
+        <Text style={style.textoLista}>{usuario.nome}</Text>
+      </View>
+    );
+  }
+
+  function Cabecalho() {
+    return (
+      <Text style={{ fontSize: 30, fontWeight: 'bold', padding: 10, textAlign: 'center' }}>Lista de Usuários</Text>
+    )
+  }
+
+  function Rodape() {
+    return (
+      <Text style={{ fontSize: 20 }}>Total de usuários: {usuarios.length}</Text>
+    )
+  }
+
+  return ( 
+    <FlatList
+      data={usuarios}
+      renderItem={ ({item}) => <Item usuario={item}/> }
+      ListHeaderComponent={ () => <Cabecalho /> }
+      ListFooterComponent={ () => <Rodape />}
+      style={{ width: '100%' }}
+    />
   );
 }
-const style = StyleSheet.create ({
- 
+
+const style = StyleSheet.create({
+  
   container: {
     flex: 1,
     justifyContent: "flex-start",
@@ -43,10 +58,11 @@ const style = StyleSheet.create ({
     width: '100%',
     padding: 10,
     marginBottom: 2,
-    backgroundColor: "blue"
+    backgroundColor: '#aaaaaa'
   },
 
-  textoLista:{
-    fontSize: 20
+  textoLista: { 
+    fontSize: 20 
   }
+
 })
